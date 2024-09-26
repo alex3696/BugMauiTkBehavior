904ad04d95738ca5bdc7e8d4c7831d35eff0c995
HOMEPC\alex3696 <alex3696@yandex.ru>
2024-09-26 17:52:10 +0700

    bugTouchBehavior - fist commit

---
+++ DebugObservableCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bugTouchBehavior
+{
+    public class DebugObservableCollection<T> : ObservableCollection<T> where T : class
+    {
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            //Console.WriteLine($"<===========Collection Count Changed: {this.Count}==============>");
+            Debug.WriteLine($"<===========Collection Count Changed: {this.Count} {e.Action}==============>");
+            base.OnCollectionChanged(e);
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace bugTouchBehavior
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Person> Persons { get; set; } = [];
+        public DebugObservableCollection<object> SelectedPersons { get; set; } = [];
+
+        public MainPage()
+        {
+            this.BindingContext = this;
+
+            InitializeComponent();
+
+            for (int i = 0; i < 5; ++i) 
+                Persons.Add(new(i));
+
+            CmdTouch = new Command<object?>(DoTouch);
+    }
+
+        public Command<object?> CmdTouch { get; }
+
+
+        void DoTouch(object? obj)
+        {
+            if(obj is Person p) 
+                Console.WriteLine(p.Id);
+        }
+    }
+
+    public record Person(int Id);
+}
